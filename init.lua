7e6407f1d0a8424b968d7a733c484b5e17f41dfd
ONELua Team <oneluaproject@gmail.com>
2018-05-28 16:05:09 -0500

    Add Edit Bubble and some fix

---
+++ init.lua
@@ -0,0 +1,175 @@
+--Init load prkxs
+__kernel, __user = false,false
+
+if files.exists("modules/kernel.skprx") then
+	if os.requirek("modules/kernel.skprx")==1 then __kernel = true end
+else
+	if os.requirek("ux0:VitaShell/module/kernel.skprx")==1 then	__kernel = true end
+end
+
+if files.exists("modules/user.suprx") then
+	if os.requireu("modules/user.suprx")==1 then __user = true end
+else
+	if os.requireu("ux0:VitaShell/module/user.suprx")==1 then __user = true end
+end
+
+--Creamos nuestra carpeta principal de Trabajo
+files.mkdir("ux0:data/ONEMENU/")
+
+__PATH_FAVS   = "ux0:data/ONEMENU/favs.txt"
+__PATH_INI    = "ux0:data/ONEMENU/config.ini"
+__PATH_THEMES = "ux0:data/ONEMENU/themes/"
+
+__FAV   = tonumber(ini.read(__PATH_INI,"favs","scan","0"))
+__THEME = ini.read(__PATH_INI,"theme","id","default")
+files.mkdir(__PATH_THEMES)
+
+__ID        = os.titleid()
+
+---------------------Buscamos icono por defecto y splash----------------------
+local splash,iconDef = nil,nil
+splash = image.load(__PATH_THEMES..__THEME.."/splash.png") or image.load("system/theme/default/splash.png")
+
+--Show Splash
+if splash then splash:blit(0,0) end
+screen.flip()
+
+iconDef = image.load(__PATH_THEMES..__THEME.."/icodef.png") or image.load("system/theme/default/icodef.png")
+------------------------Checamos la lista de Favoritos------------------------
+apps = {}
+function write_favs(pathini)
+    local file = io.open(pathini, "w+")
+    file:write("apps = {\n")
+
+	for i=1,#apps do
+		if i==#apps then
+			file:write(string.format('"%s"\n', tostring(apps[i])))
+		else
+			file:write(string.format('"%s",\n', tostring(apps[i])))
+		end
+	end
+	file:write("}\n")
+	file:close()
+end
+
+if files.exists(__PATH_FAVS) then dofile(__PATH_FAVS) else
+write_favs(__PATH_FAVS) end
+------------------------Checamos la lista de Favoritos------------------------
+
+------------------Busqueda y peticion de Iconos en modo hilo------------------
+__CATEGORIES = 5
+appman = {}
+for i=1,__CATEGORIES do table.insert(appman, { list={}, scroll, sort = 0, slide = { img = nil, x=0 , acel=7, w= 0 } } ) end
+cat, appman.len = 0,0
+
+IMAGE_PORT_I = channel.new("IMAGE_PORT_I")
+IMAGE_PORT_O = channel.new("IMAGE_PORT_O")
+THID_IMAGE = thread.new("system/appmanager/thread_img.lua")
+
+static_void = {}
+for i=1,__CATEGORIES do static_void[i] = {x=1} end
+
+function fillappman(obj)
+
+	if obj.id == __ID then return end
+
+	local index=1
+
+	if obj.type == "mb" then
+		index = 3
+		obj.resize = true
+		obj.path_img = "ur0:appmeta/"..obj.id.."/pic0.png"
+	elseif obj.type == "EG" or obj.type == "ME" then
+		index = 4
+		obj.resize = true
+		obj.path_img = "ur0:appmeta/"..obj.id.."/livearea/contents/startup.png"
+	else
+
+		if files.exists(obj.path.."/data/boot.inf") or obj.id == "PSPEMUCFW" then
+			index = 5
+		else
+			local region = "N"
+			local sfo = game.info(obj.path.."/sce_sys/param.sfo")
+			if sfo and sfo.CONTENT_ID then
+				if sfo.CONTENT_ID:len() > 9 then index = 1 else	index = 2 end
+				region = sfo.CONTENT_ID[1]
+			else
+				index = 2
+			end
+
+		end
+
+		obj.path_img = "ur0:appmeta/"..obj.id.."/icon0.png"
+
+	end
+	obj.region = region or "N"
+
+	obj.img = iconDef
+	if __FAV == 1 then
+		obj.img:resize(120,120)
+	else
+		if obj.resize then obj.img:resize(120,100) else obj.img:resize(120,120) end
+	end
+	obj.img:setfilter(__IMG_FILTER_LINEAR, __IMG_FILTER_LINEAR)
+
+	appman.len += 1
+
+	table.insert(appman[index].list,obj)
+
+end
+
+function Scanning()
+
+	-- Init with Max CPU/GPU
+	__CPU = os.cpu()
+	os.cpu(444)
+	__GPU = os.gpuclock()
+	os.gpuclock(166)
+
+	--id, type, version, dev, path, title
+	local list = game.list(__GAME_LIST_ALL)
+	local sort_vita = tonumber(ini.read(__PATH_INI,"sort","sort","0"))
+	if sort_vita == 1 then
+		table.sort(list, function (a,b) return string.lower(a.title)<string.lower(b.title) end)
+	else
+		table.sort(list, function (a,b) return string.lower(a.id)<string.lower(b.id) end)
+	end
+
+	for i=1,#list do
+
+		if files.exists(list[i].path) then
+			if list[i].title then list[i].title = list[i].title:gsub("\n"," ") end
+			list[i].fav = false
+			for j=1,#apps do
+				if list[i].id == apps[j] then list[i].fav = true end
+			end
+
+			if __FAV == 1 then
+				if list[i].fav then fillappman(list[i]) end--Scan only Favs
+			else
+				fillappman(list[i])
+			end
+		end
+
+	end
+
+	local y,x = 1,1
+	while y <= __CATEGORIES do
+		if cat == 0 and appman[y].list[x] then cat = y end
+		while appman[y].list[x] do
+			-- Push request of icon! :D
+			local obj = appman[y].list[x]
+			static_void[y][x] = obj
+			IMAGE_PORT_O:push( { x = x, y = y, fav = __FAV, path = obj.path_img, resize = obj.resize } )
+			x += 1
+		end
+		y += 1
+		x = 1
+	end
+
+	if cat <= 0 then appman.len = 0 end
+
+end
+------------------Busqueda y peticion de Iconos en modo hilo------------------
+
+Scanning()
---
+++ system/explorer/refresh.lua
@@ -0,0 +1,147 @@
+--[[ 
+	ONEMenu
+	Application, themes and files manager.
+	
+	Licensed by Creative Commons Attribution-ShareAlike 4.0
+	http://creativecommons.org/licenses/by-sa/4.0/
+	
+	Designed By Gdljjrod & DevDavisNunez.
+	Collaborators: BaltazaR4 & Wzjk.
+]]
+
+function fillapps(list, obj)
+
+	local info = game.info(obj.path.."/sce_sys/param.sfo")
+	if info then
+		if info.TITLE_ID and info.CATEGORY != "gda" then
+
+			if info.TITLE then info.TITLE = info.TITLE:gsub("\n"," ") else info.TITLE = "UNK" end
+
+			local resize = false
+			if info.CATEGORY == "mb" or info.CATEGORY == "EG" or info.CATEGORY == "ME" then
+				resize = true
+			end
+
+			local index = 1
+			if files.exists(obj.path.."/data/boot.inf") or obj.id == "PSPEMUCFW" then index = 5
+			else
+				if info.CONTENT_ID and info.CONTENT_ID:len() > 9 then index = 1 else index = 2 end
+			end
+
+			table.insert(list, { id=info.TITLE_ID, type=info.CATEGORY, version=info.APP_VER or "00.00", title=info.TITLE or info.TITLE_ID,
+								 path=obj.path, index = index, resize = resize })
+		end
+	end
+
+end
+
+function refresh_init(img)
+
+	if img then img:blit(0,0) end
+	message_wait()
+	os.delay(15)
+
+	local vbuff = screen.toimage()
+	if vbuff then vbuff:blit(0,0) elseif img then img:blit(0,0) end
+
+	local list = {}
+	--Scanning ux0:app
+	local tmp = files.listdirs("ux0:app")
+	if tmp and #tmp > 0 then
+		table.sort(tmp ,function (a,b) return string.lower(a.name)<string.lower(b.name) end)
+		for i=1, #tmp do
+			if game.exists(tmp[i].name) then
+				if not game.rif(tmp[i].name) and not game.frif(tmp[i].name) then
+					fillapps(list, tmp[i])
+				end
+			else
+				fillapps(list, tmp[i])
+			end
+		end
+	end
+
+	--Installing
+	if #list > 0 then
+		local count = 0
+		for i=1, #list do
+
+			if img then img:blit(0,0)	end
+			__TITTLEAPP, __IDAPP = list[i].title, list[i].id
+
+			buttons.homepopup(0)
+				local result = game.refresh(list[i].path)
+			buttons.homepopup(1)
+
+			if result == 1 then
+
+				count += 1
+				list[i].dev = "ux0:"
+
+				--Size
+				list[i].size = files.size(list[i].path)
+				list[i].sizef = files.sizeformat(list[i].size or 0)
+ 
+				--Update appman[].list
+				local icon0 = image.load("ur0:appmeta/"..list[i].id.."/icon0.png")
+				if icon0 then
+					list[i].img = icon0
+					if __FAV == 1 then
+						list[i].img:resize(120,120)
+					else
+						if list[i].resize then
+							list[i].img:resize(120,100)
+						else
+							list[i].img:resize(120,120)
+						end
+					end
+					list[i].img:setfilter(__IMG_FILTER_LINEAR, __IMG_FILTER_LINEAR)
+				else
+					list[i].img = theme.data["icodef"]
+				end
+
+				--Search game in appman[index].list
+				local search = 0
+				for j=1,appman[list[i].index].scroll.maxim do
+					if list[i].id == appman[list[i].index].list[j].id then search = j break end
+				end
+
+				if search == 0 then
+					if __FAV == 0 then
+						list[i].fav = false
+						table.insert(appman[list[i].index].list, list[i])
+						if appman[list[i].index].sort == 1 then
+							table.sort(appman[list[i].index].list ,function (a,b) return string.lower(a.title)<string.lower(b.title) end)
+						else
+							table.sort(appman[list[i].index].list ,function (a,b) return string.lower(a.id)<string.lower(b.id) end)
+						end
+						appman[list[i].index].scroll:set(appman[list[i].index].list,limit)
+					end
+				else
+					--update
+					appman[list[i].index].list[search].dev = "ux0:"
+					appman[list[i].index].list[search].img = list[i].img
+
+					appman[list[i].index].list[search].type = list[i].type
+					appman[list[i].index].list[search].version = list[i].version
+					appman[list[i].index].list[search].title = list[i].title
+
+				end
+
+				appman.len +=1
+				infodevices()
+
+			else
+				os.message(strings.notinstalled..list[i].id)
+			end
+		end
+		os.message(strings.gamesinst..count)
+
+	else
+		os.message(strings.nogames)
+	end
+
+	os.delay(15)
+	if vbuff then vbuff:blit(0,0) elseif img then img:blit(0,0) end
+	__TITTLEAPP, __IDAPP = "",""
+
+end
